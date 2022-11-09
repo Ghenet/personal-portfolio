eadcd42f0de23c6653553f37e883cf017c34ae2c
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-11-09 15:28:24 -0800

    adds footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+// import { Container, Row, Col } from "react-bootstrap";
+// import navIcon1 from '../assets/img/nav-icon1.svg';
+// import navIcon2 from '../assets/img/nav-icon2.svg';
+// import navIcon3 from '../assets/img/nav-icon3.svg';
+// import navIcon4 from '../assets/img/nav-icon4.svg';
+
+// export const Footer = () => {
+//     return (
+//         <Footer className="footer">
+//             <Container>
+//                 <Row className="align-item-center">
+//                     <a href="mailto:ghenet.andemichael@gmail.com">ghenet.andemichael@gmail.com</a>
+//                     <Col sm={6} className="text-center text-sm-end">
+//                         <div className="social-icon">
+//                         <a href="https://www.linkedin.com/in/ghenet-andemichael/"><img src={navIcon1} alt="linkedin" /></a>
+//                         <a href="https://github.com/Ghenet"><img src={navIcon2} alt="Github" /></a>
+//                         <a href="https://codepen.io/Gheni"><img src={navIcon3} alt="codepen" /></a>
+//                         <a href="https://twitter.com/Ghenikibrom"><img src={navIcon4} alt="twitter" /></a>
+//                         </div>
+
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </Footer>
+//     )
+// }
