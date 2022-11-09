d13c80267da6fd7dbfef18e923f9486a587ad92c
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-11-09 15:27:37 -0800

    imports iconsImgs and adds links to the social icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import {useState, useEffect } from 'react';
 import { Container, Nav, Navbar } from "react-bootstrap";
-import logo from '.../assets/img/logo.svg';
-import navIcon1 from '.../assets/img/nav-icon1.svg';
-import navIcon2 from '.../assets/img/nav-icon2.svg';
-import navIcon3 from '.../assets/img/nav-icon3.svg';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon4.svg';
 
 
 
@@ -49,14 +50,19 @@ const onUpdateActiveLink = (value) => {
                 </Nav>
                 <span className="navbar-text">
                     <div className="social-icon">
-                        <a href="#"><img src={navIcon1} alt="" /></a>
-                        <a href="#"><img src={navIcon2} alt="" /></a>
-                        <a href="#"><img src={navIcon3} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/ghenet-andemichael/"><img src={navIcon1} alt="linkedin" /></a>
+                        <a href="https://github.com/Ghenet"><img src={navIcon2} alt="Github" /></a>
+                        <a href="https://codepen.io/Gheni"><img src={navIcon3} alt="codepen" /></a>
+                        <a href="https://twitter.com/Ghenikibrom"><img src={navIcon4} alt="twitter" /></a>
                     </div>
-                    <button className="vvd" onClick={()=>console.log('connect')}><span>Lets Connect!</span></button>
+                    <button className="vvd" onClick={()=>console.log('connect')}><span><a href="mailto:ghenet.andemichael@gmail.com">Let's Connect!</a></span></button>
                 </span>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
   );
 }
+
+
+//Future enhancement
+// change the logo with my anime pic
