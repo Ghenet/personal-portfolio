30d0be20641d83991a7d25df97ec5ba44af31df4
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-11-09 15:29:45 -0800

    adds alt desc for the project imgs

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -1,10 +1,10 @@
 import { Col }  from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectsCard = ({ title, description, imgUrl }) => {
     return (
         <Col sm={6} md={4}>
             <div className="proj-imgbx">
-                <img src={imgUrl} /> 
+                <img src={imgUrl} alt="img of the project" /> 
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
